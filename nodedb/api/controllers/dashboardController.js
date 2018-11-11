eda84c17068692840da8a749c5e7c0b8d50ae08e
Jacopo Vendramin <vendraminjacopo@gmail.com>
2018-11-11 02:43:05 +0000

    fs

--- nodedb/api/controllers/dashboardController.js
+++ nodedb/api/controllers/dashboardController.js
@@ -1,6 +1,23 @@
 "use strict";
 
+const mongoose = require("mongoose")
+const Station = mongoose.model("RailData")
 
-var mongoose = require("mongoose"),
-    Task = mongoose.model("Tasks"),
-    Station = mongoose.model("RailData");
+class DashboardController {
+
+    async test (req, res) {
+        var new_task = new Station({
+            Date: 1491177600000,
+            "TimeIn": 0.0,
+            Station: "BEDHAMPTON",
+            Headcode: "7Z75",
+            Trainperf: -3.0
+        })
+        let ciao = await new_task.save().then((err)=>{
+            if (err) res.send(err);
+            res.json(task);
+        })
+        return ciao
+    }
+}
+module.exports = DashboardController
--- nodedb/api/routes/nodedbRoutes.js
+++ nodedb/api/routes/nodedbRoutes.js
@@ -1,17 +1,21 @@
 "use strict";
-module.exports = function(app) {
+module.exports = function (app) {
     var ndb = require("../controllers/nodedbController");
+    const DashboardController = require("../controllers/DashboardController")
 
     // todoList Routes
     app.route("/tasks")
         .get(ndb.list_all_tasks)
-        .post(ndb.create_a_task);
+        .post(ndb.create_a_task)
 
     app.route("/tasks/:taskId")
         .get(ndb.read_a_task)
         .put(ndb.update_a_task)
-        .delete(ndb.delete_a_task);
+        .delete(ndb.delete_a_task)
 
     app.route("/test")
-        .get(ndb.test);
+        .get(ndb.seedDatabase)
+    // DashboardController.test()
+
+
 };
